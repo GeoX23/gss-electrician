1cd622677374480d6240e44b2384413a1c419e10
George Chervatidis <giwrgos.chervatidis@gmail.com>
2024-09-06 22:30:42 +0300

    Vercel Analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 import { PopupWidget } from "@/components/PopupWidget";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
           <Footer />
           <PopupWidget />
           <SpeedInsights />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
